1ba9af66b2b297802d524e934e60da954e283cd0
timmyeb <timmyebb@gmail.com>
2020-07-04 19:17:30 +0200

    rename to .sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Find all dot files then if the original file exists, create a backup
+# Once backed up to {file}.dtbak symlink the new dotfile in place
+for file in $(find . -maxdepth 1 -name ".*" -type f  -printf "%f\n" ); do
+    if [ -e ~/$file ]; then
+        mv -f ~/$file{,.dtbak}
+    fi
+    ln -s $PWD/$file ~/$file
+done
+
+# Install zsh theme 
+if [ -d "$HOME/.oh-my-zsh" ]; then
+	cp ./oh-my-zsh/themes/telf.zsh-theme ~/.oh-my-zsh/themes/telf.zsh-theme
+fi
+
+echo "Installed"
